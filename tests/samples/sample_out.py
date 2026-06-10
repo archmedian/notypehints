471cafdc0843e9873c98b2ee2973199b6ecbcd0d
Andy Agent <andyagent445@gmail.com>
2026-06-10 23:44:35 +0000

    create sample files

---
+++ tests/samples/sample_out.py
@@ -0,0 +1,122 @@
+IntList = list
+
+
+class Person:
+    pass
+
+class Point:
+    pass
+
+class Drawable:
+    def draw(self): ...
+
+class Container:
+    def __init__(self, value):
+        self.value = value
+
+
+def func_int(x):
+    pass
+
+
+def func_str(x):
+    pass
+
+
+def func_float(x):
+    pass
+
+
+def func_bool(x):
+    pass
+
+
+def func_bytes(x):
+    pass
+
+
+def func_list(x):
+    pass
+
+
+def func_dict(x):
+    pass
+
+
+def func_set(x):
+    pass
+
+
+def func_tuple(x):
+    pass
+
+
+def func_optional(x):
+    pass
+
+
+def func_union(x):
+    pass
+
+
+def func_callable(x):
+    pass
+
+
+def func_literal(x):
+    pass
+
+
+CONST = 1
+
+
+class MyClass:
+    var = 2
+
+
+def func_annotated(x):
+    pass
+
+
+IntListAlias = list
+
+
+def func_typealias(x):
+    pass
+
+
+def func_typevar(x):
+    return x
+
+
+def func_protocol(x):
+    pass
+
+
+def func_typeddict(x):
+    pass
+
+
+def func_namedtuple(x):
+    pass
+
+
+def func_generic(x):
+    pass
+
+
+def func_nested_list(x):
+    pass
+
+
+def func_nested_dict(x):
+    pass
+
+
+def func_nested_tuple(x):
+    pass
+
+
+def func_none():
+    pass
+
